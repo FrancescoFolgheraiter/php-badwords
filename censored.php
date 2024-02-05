5ba182db74040fd4b19ef31a88a095b6de14762f
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-05 16:05:16 +0100

    add some style

--- censored.php
+++ censored.php
@@ -10,14 +10,17 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>php-badwords</title>
+        <!--boostrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body>
-        <h2>
+        <h2 class="text-center">
             Risultato
         </h2>
         <div>
         <?php
         echo $text;
+        echo "<br>";
         echo "Lunghezza del paragrafo pari a :".strlen($text);
         ?>
 
@@ -25,6 +28,7 @@
         <div>
         <?php
         echo str_replace($word, '***', $text);
+        echo "<br>";
         echo "Lunghezza del paragrafo censurato con la parola: ".$word." è di: ".strlen(str_replace($word, '***', $text));
         ?>
         </div>
--- index.php
+++ index.php
@@ -5,20 +5,22 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>php-badwords</title>
+        <!--boostrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body> 
-        <h1>
+        <h1 class="text-center">
             Php Badwords
         </h1>
-        <div>
+        <div class="container">
             <form action="./censored.php" method="GET">
-                <div>
-                    <label for="paragraph">Paragrafo:</label>
-                    <textarea name="paragraph" id="" cols="45" rows="10" placeholder="Inserisci un paragrafo"></textarea>
+                <div class="d-flex align-items-center mb-3">
+                    <label class="me-3" for="paragraph">Paragrafo:</label>
+                    <textarea name="paragraph" id="paragraph" cols="45" rows="10" placeholder="Inserisci un paragrafo"></textarea>
                 </div>
-                <div>
-                    <label for="censuredWord">Parola:</label>
-                    <input type="text" placeholder="Inserisci una parola da censurare" name="censuredWord">
+                <div class="d-flex align-items-center mb-3">
+                    <label class="me-3" for="censuredWord">Parola:</label>
+                    <input type="text" placeholder="Inserisci una parola da censurare" name="censuredWord" id="censuredWord">
                 </div>
                 <button type="submit">
                     Censura
